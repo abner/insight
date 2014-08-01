2f682e43edb4e657896a6e1d7c1ce68480b3eb73
Ábner Oliveira <abner.silva@gmail.com>
2014-08-01 10:23:16 +0000

    gemfile mais completo

--- muli_platform_selector.rb
+++ muli_platform_selector.rb
@@ -5,3 +5,44 @@ end
 platforms :mri do
   gem "sqlite3"
 end
+
+#===================================
+
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+
+group :development do
+  gem 'pry-rails'
+  platforms :ruby do
+    gem 'rb-readline', '~> 0.5.0.pre.1', :require => false
+    gem 'sqlite3'
+  end
+end
+
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'turbolinks'
+gem 'jquery-rails'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+group :production do
+  gem 'puma'
+
+  platforms :jruby do
+    gem 'activerecord-jdbc-adapter'
+    gem 'jdbc-mysql'
+  end
+
+  platforms :ruby do
+    gem 'mysql2'
+
+    # gem for webserver is required for application using bundler
+    gem 'thin'
+    gem 'unicorn'
+  end
+end
