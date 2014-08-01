6c87295fbcacd0353bd4f1d653d08aab22c5aff3
Ábner Silva de Oliveira <abner.silva@gmail.com>
2014-08-01 06:53:40 -0300

    initializer to fix error 'OpenSSL::Cipher::CipherError (Illegal key size: possibly you need to install Java Cryptography Extension (JCE) Unlimited Strength Jurisdiction Policy Files for your JRE)'

---
+++ config/initializers/jce_unlimit_strength.rb
@@ -0,0 +1,6 @@
+if defined?(RUBY_ENGINE) && RUBY_ENGINE == 'jruby'
+  security_class = java.lang.Class.for_name('javax.crypto.JceSecurity')
+  restricted_field = security_class.get_declared_field('isRestricted')
+  restricted_field.accessible = true
+  restricted_field.set nil, false
+end
