b8ce7bae106dc5cf55e7fc89f6b8dbbf0f01fce2
Ábner Silva de Oliveira <abner.silva@gmail.com>
2014-08-15 07:38:23 -0300

    changes on feedback model

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,10 +1,19 @@
 class Feedback
   include Mongoid::Document
-  include Mongoid::Attributes::Dynamic
+  #include Mongoid::Attributes::Dynamic
 
-  belongs_to :project
+  belongs_to :webapp
 
   field :datetime, type: DateTime
+
+  field :ip, type:String
+  field :browser, type: String
+  
+
+  field :category, type: String
+
+  field :screenshot_path, type: String
+
 
 
 end
