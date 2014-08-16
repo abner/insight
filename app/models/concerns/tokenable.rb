3ae447576e57416c77c3e807189f74d04b2b3318
Ábner Silva de Oliveira <abner.silva@gmail.com>
2014-08-15 23:33:22 -0300

    changed to tokenable use SecureRandom.urlsafe_base64 instead of SecureRandom.uuid

--- app/models/concerns/tokenable.rb
+++ app/models/concerns/tokenable.rb
@@ -14,7 +14,8 @@ module Tokenable
   def generate_token
     self.authentication_token = loop do
       #random_token = SecureRandom.urlsafe_base64(nil, false)
-      random_token = SecureRandom.uuid
+      #random_token = SecureRandom.uuid
+      random_token = SecureRandom.urlsafe_base64
       break random_token unless self.class.where(authentication_token: random_token).exists?
     end
   end
