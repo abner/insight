da8b0d8ffdc6c1d6981d94ca272f28f00bc97e69
root <root@psocial.sg.gov.br>
2015-02-04 12:06:41 -0200

    fixing digest on assets url configuration for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Generate digests for assets URLs.
-  config.assets.digest = false
+  config.assets.digest = true
 
   # `config.assets.precompile` has moved to config/initializers/assets.rb
 
