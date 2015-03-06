c0a12a8e0d6bf38bd05c0ee937cd84ac269192c3
root <root@psocial.sg.gov.br>
2015-03-06 17:54:55 -0300

    changed run.sh to protect shared key base

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export SECRET_KEY_BASE=7351707e8086f732bc3c2661b162c68589ce2194ec94fd08565946f2a1ee2df8aa66b94c9452777829114d4bc7ec114acd7c55a45aa5b1326acf649c648dfe28
 cd /opt/feedback-server/
+source  ../feedback-shared/secret_export
 bundle exec puma -b tcp://127.0.0.1 -e production -p 3232 -d  --pidfile /var/run/feedback-web/feedback-web.pid
