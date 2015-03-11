da5540f28ba4ded91c6c7ad8bd4ae641518cfc25
Ábner Silva de Oliveira <abner.oliveira@serpro.gov.br>
2015-03-11 11:02:37 -0300

    added ci environment

--- db/migrate/20150304143715_update_templates_on_existing_apps.rb
+++ db/migrate/20150304143715_update_templates_on_existing_apps.rb
@@ -1,24 +1,34 @@
 class UpdateTemplatesOnExistingApps < Mongoid::Migration
+  def self.check_class
+    begin
+      UserApplication
+    rescue
+      return false
+    end
+    true
+  end
   def self.up
-    UserApplication.all.each do |app|
-      puts "Processando app #{app.name}"
-      if app.default_feedback_form.nil?
-        puts 'app com default form nil'
-        if app.feedback_forms.empty?
-          puts 'não tem form - criando um'
-          form_attributes = FeedbackFormTemplate.default_template.attributes_template
-          form = app.feedback_forms.create! form_attributes
-          #set as default form for this user_application
-          puts 'definindo form'
-          app.set_default_form! form
-        else
-          puts 'já tinha form - definindo primeiro form como default'
-          puts "#{app.feedback_forms.first.inspect}"
-          app.set_default_form! app.feedback_forms.first
+    if check_class
+      UserApplication.all.each do |app|
+        puts "Processando app #{app.name}"
+        if app.default_feedback_form.nil?
+          puts 'app com default form nil'
+          if app.feedback_forms.empty?
+            puts 'não tem form - criando um'
+            form_attributes = FeedbackFormTemplate.default_template.attributes_template
+            form = app.feedback_forms.create! form_attributes
+            #set as default form for this user_application
+            puts 'definindo form'
+            app.set_default_form! form
+          else
+            puts 'já tinha form - definindo primeiro form como default'
+            puts "#{app.feedback_forms.first.inspect}"
+            app.set_default_form! app.feedback_forms.first
+          end
+          puts "Defaut form gravado: #{app.default_feedback_form.inspect}"
+          app.reload
+          puts "Defaut form gravado #after_reload: #{app.default_feedback_form.inspect}"
         end
-        puts "Defaut form gravado: #{app.default_feedback_form.inspect}"
-        app.reload
-        puts "Defaut form gravado #after_reload: #{app.default_feedback_form.inspect}"
       end
     end
   end
--- db/migrate/20150304143735_transfer_feedbacks_from_user_applications_to_default_form.rb
+++ db/migrate/20150304143735_transfer_feedbacks_from_user_applications_to_default_form.rb
@@ -1,9 +1,19 @@
 class TransferFeedbacksFromUserApplicationsToDefaultForm < Mongoid::Migration
+  def self.check_class
+    begin
+      UserApplication
+    rescue
+      return false
+    end
+    true
+  end
   def self.up
-    UserApplication.all.each do |app|
-      if(app.default_feedback_form.feedbacks.empty? && app.feedbacks.count > 0)
-        app.feedbacks.each do |feedback|
-          feedback.update_attribute(:feedback_form, app.default_feedback_form) if feedback.feedback_form.nil?
+    if check_class
+      UserApplication.all.each do |app|
+        if(app.default_feedback_form.feedbacks.empty? && app.feedbacks.count > 0)
+          app.feedbacks.each do |feedback|
+            feedback.update_attribute(:feedback_form, app.default_feedback_form) if feedback.feedback_form.nil?
+          end
         end
       end
     end
